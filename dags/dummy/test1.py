8d3ab0e00f95ac8c3fbf675bb9bd761d6491747e
Nikita Fomin <defdXs@yandex.ru>
2025-04-27 13:46:02 +0300

    Abobabebebe

---
+++ dags/dummy/test1.py
@@ -0,0 +1,34 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+import logging
+from airflow.utils.dates import days_ago
+
+DEFAULT_ARGS = {
+  'owner': 'aexra'
+}
+
+with DAG(
+  dag_id="test1",
+  description="my first dag",
+  tags=["kaka"],
+  schedule_interval='@daily',
+  start_date=days_ago(2),
+  default_args=DEFAULT_ARGS,
+  max_active_runs=1
+) as dag:
+  def i_need_a_bullets():
+    logging.warning('I NEED A BULLETS')
+
+  task1 = PythonOperator(
+    python_callable=i_need_a_bullets,
+    task_id="aboba"
+  )
+
+  task2 = BashOperator(
+    bash_command='''echo "Hello, world!"''',
+    task_id="bebebe"
+  )
+
+  task1 >> task2
+
