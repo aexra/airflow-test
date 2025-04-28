02ceaa7c65067adbf6283575662a8d63f7cfecab
Nikita Fomin <defdXs@yandex.ru>
2025-04-28 17:02:54 +0300

    Random not workin

--- dags/cars_exam/refill_cars.py
+++ dags/cars_exam/refill_cars.py
@@ -1,11 +1,12 @@
 from airflow.decorators import dag, task
 from airflow.utils.dates import days_ago
 from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 import logging
 import numpy as np
 import pandas as pd
 
-CARS_PER_REFILL=5
+CARS_PER_REFILL=10
 
 @dag(
     dag_id="refill_cars",
@@ -41,11 +42,35 @@ def refill_cars():
   @task
   def transform_cars(cars):
     cars['price_usd'] = np.random.randint(10_000, 50_000, size=cars.shape[0])
+    cars['id'] = range(1, cars.shape[0] + 1)
     return cars
 
   @task
-  def load_cars(cars):
-    logging.info(f"\n----------------------\n{cars}\n----------------------")
+  def load_cars(cars: pd.DataFrame):
+    hook = PostgresHook(postgres_conn_id="postgres_conn")
+    conn = hook.get_conn()
+    cur = conn.cursor()
+
+    try:
+        query = f"""
+        TRUNCATE TABLE cars;
+        INSERT INTO cars (id, mark, model, engine_volume, published_at, price_usd) VALUES
+        {",\n".join([f"({c['id']}, '{c['mark']}', '{c['model']}', {c['engine_volume']}, {c['published_at']}, {c['price_usd']})" for i, c in cars.iterrows()])};
+        """
+        
+        print("Executing query:", query)
+        
+        cur.execute(query)
+        conn.commit()
+        
+        logging.info(f"Successfully inserted {len(cars)} records")
+    except Exception as e:
+        conn.rollback()
+        logging.error(f"Error inserting data: {str(e)}")
+        raise
+    finally:
+        cur.close()
+        conn.close()
 
   extract = extract_random_cars()
   transform = transform_cars(extract)
