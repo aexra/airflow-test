639887c8ff5d85fc93fc564145b7481bd5fe3fb2
Nikita Fomin <defdXs@yandex.ru>
2025-05-10 13:44:27 +0300

    Seeding gp

--- init-gp/init.sql
+++ init-gp/init.sql
@@ -1,3 +1,23 @@
-CREATE TABLE IF NOT EXISTS Aboba (
-	id serial PRIMARY KEY
+CREATE TABLE IF NOT EXISTS dim_cars (
+	car_sk serial PRIMARY KEY,
+	mark text NOT NULL,
+	model text NOT NULL,
+	engine_volume text NOT NULL,
+	year_of_manufacture integer NOT NULL,
+	last_modified timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+)
+
+CREATE TABLE IF NOT EXISTS fact_cars (
+	sk serial PRIMARY KEY,
+	car_sk integer NOT NULL,
+	price_foreign numeric NOT NULL,
+	corrency_code_foreign text NOT NULL,
+	price_rub numeric NOT NULL,
+	ex_rate numeric NOT NULL,
+	effective_from timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	effective_to timestamp NOT NULL,
+	is_current boolean NOT NULL,
+	source_filename text NOT NULL,
+	load_ts timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	CONSTRAINT fk_car_sk FOREIGN KEY (car_sk) REFERENCES dim_cars(car_sk)
 )
